17926910c2a72e3a6c58ae6703d08e775cc692ca
Speechrezz <thespeechlessmusic@gmail.com>
2025-01-23 10:55:24 -0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<div class="w-full grid grid-cols-5 bg-[url(/img/diamond.jpg)] bg-repeat bg-200 min-h-screen">
+  <div class="h-full col-span-1 bg-[url(/img/pickaxe.gif)] bg-repeat-y bg-contain"></div>
+  <main class="col-span-3 max-w-2xl mx-auto">
+    <!-- Header -->
+    <div class="flex flex-col justify-center text-center header-text bg-[url(/img/buff_mark.jpg)] bg-contain aspect-[754/960] px-4">
+      <h1 class="text-6xl sm:text-8xl pt-8 sm:pt-32">
+        Cuck SMP©
+      </h1>
+      <h2 class="text-3xl mt-8">
+        In Cuck We Trust
+      </h2>
+    </div>
+
+    <!-- Join the discord link -->
+    <a href="https://discord.gg/PJEPxakEFc">
+      <img src="img/join-the-discord.gif" alt="Join the discord" class="hover:skew-x-[40deg] transition-transform duration-300">
+    </a>
+  </main>
+  <div class="h-full col-span-1 bg-[url(/img/pickaxe.gif)] bg-repeat-y bg-contain"></div>
+</div>
+
+<style lang="postcss">
+  .header-text {
+    text-shadow: 2px 2px red;
+    @apply text-white;
+  }
+
+  .bg-200 {
+    background-size: 200px;
+  }
+</style>
